5b98f3f702b2eedf4dcb09ec2c4fa32a1b497c25
Greg Willard <Wickett06@gmail.com>
2016-05-17 19:47:33 -0400

    mach-msm: display: revert a11 back to what it was originally
    Well, plus a couple minor fixes to let it compile

--- arch/arm/mach-msm/display/htc_a11-panel.c
+++ arch/arm/mach-msm/display/htc_a11-panel.c
@@ -9,20 +9,16 @@
 #include <mach/debug_display.h>
 #include "../../../../drivers/video/msm/mdss/mdss_dsi.h"
 
-#define PANEL_ID_KIWI_SHARP_HX      0
+#define PANEL_ID_KIWI_SHARP_HX      	0
 #define PANEL_ID_A5_JDI_NT35521_C3      1
 
-/* HTC: dsi_power_data overwrite the role of dsi_drv_cm_data
-   in mdss_dsi_ctrl_pdata structure */
 struct dsi_power_data {
-	uint32_t sysrev;         /* system revision info */
-	struct regulator *vddio; /* 1.8v */
-	struct regulator *vdda;  /* 1.2v */
-	struct regulator *vdd;  /* 3v */
-	struct regulator *vddpll; /* 1.8v */
-	int lcmio;
-	int lcmp5v;
-	int lcmn5v;
+	uint32_t sysrev;         
+	struct regulator *vddio; 
+	struct regulator *vdda;  
+	struct regulator *vddpll; 
+	struct regulator *vlcm3v; 
+
 	int lcm_bl_en;
 };
 
@@ -44,7 +40,7 @@ struct i2c_dev_info {
 	{.dev_addr = addr >> 1, .client = NULL}
 
 static struct i2c_dev_info device_addresses[] = {
-	I2C_DEV_INFO(0x7C)
+	I2C_DEV_INFO(0x6C)
 };
 
 static inline int platform_write_i2c_block(struct i2c_adapter *i2c_bus
@@ -57,7 +53,6 @@ static inline int platform_write_i2c_block(struct i2c_adapter *i2c_bus
 	struct i2c_msg msg;
 	u8 *buffer;
 	int ret;
-
 	buffer = kmalloc(count + 1, GFP_KERNEL);
 	if (!buffer) {
 		printk("%s:%d buffer allocation failed\n",__FUNCTION__,__LINE__);
@@ -93,7 +88,7 @@ static int tps_65132_add_i2c(struct i2c_client *client)
 	struct i2c_adapter *adapter = client->adapter;
 	int idx;
 
-	/* "Hotplug" the MHL transmitter device onto the 2nd I2C bus  for BB-xM or 4th for pandaboard*/
+	
 	i2c_bus_adapter = adapter;
 	if (i2c_bus_adapter == NULL) {
 		PR_DISP_ERR("%s() failed to get i2c adapter\n", __func__);
@@ -171,95 +166,84 @@ static int htc_a11_regulator_init(struct platform_device *pdev)
 
 	PR_DISP_INFO("%s\n", __func__);
 	if (!pdev) {
-		PR_DISP_ERR("%s: invalid input\n", __func__);
+		pr_err("%s: invalid input\n", __func__);
 		return -EINVAL;
 	}
 
 	ctrl_pdata = platform_get_drvdata(pdev);
 	if (!ctrl_pdata) {
-		PR_DISP_ERR("%s: invalid driver data\n", __func__);
+		pr_err("%s: invalid driver data\n", __func__);
 		return -EINVAL;
 	}
 
 	pwrdata = devm_kzalloc(&pdev->dev,
 				sizeof(struct dsi_power_data), GFP_KERNEL);
 	if (!pwrdata) {
-		PR_DISP_ERR("%s: FAILED to alloc pwrdata\n", __func__);
+		pr_err("[DISP] %s: FAILED to alloc pwrdata\n", __func__);
 		return -ENOMEM;
 	}
 
 	ctrl_pdata->dsi_pwrctrl_data = pwrdata;
 
-	pwrdata->vdd = devm_regulator_get(&pdev->dev, "vdd");
-	if (IS_ERR(pwrdata->vdd)) {
-		PR_DISP_ERR("%s: could not get vdd reg, rc=%ld\n",
-			__func__, PTR_ERR(pwrdata->vdd));
-		return PTR_ERR(pwrdata->vdd);
-	}
-
 	pwrdata->vddio = devm_regulator_get(&pdev->dev, "vddio");
 	if (IS_ERR(pwrdata->vddio)) {
-		PR_DISP_ERR("%s: could not get vddio reg, rc=%ld\n",
+		pr_err("%s: could not get vddio reg, rc=%ld\n",
 			__func__, PTR_ERR(pwrdata->vddio));
 		return PTR_ERR(pwrdata->vddio);
 	}
 
-	pwrdata->vdda = devm_regulator_get(&pdev->dev, "vdda");
-	if (IS_ERR(pwrdata->vdda)) {
-		PR_DISP_ERR("%s: could not get vdda vreg, rc=%ld\n",
-			__func__, PTR_ERR(pwrdata->vdda));
-		return PTR_ERR(pwrdata->vdda);
+	pwrdata->vlcm3v = devm_regulator_get(&pdev->dev, "vdd");
+	if (IS_ERR(pwrdata->vlcm3v)) {
+		pr_err("%s: could not get vdd reg, rc=%ld\n",
+			__func__, PTR_ERR(pwrdata->vlcm3v));
+		return PTR_ERR(pwrdata->vlcm3v);
 	}
 
-	ret = regulator_set_voltage(pwrdata->vdd, 3000000,
-		3000000);
-	if (ret) {
-	    PR_DISP_ERR("%s: set voltage failed on vdd vreg, rc=%d\n",
-	        __func__, ret);
-	    return ret;
+	pwrdata->vddpll = devm_regulator_get(&pdev->dev, "vddpll");
+	if (IS_ERR(pwrdata->vddpll)) {
+		pr_err("%s: could not get vddpll reg, rc=%ld\n",
+			__func__, PTR_ERR(pwrdata->vddpll));
+		return PTR_ERR(pwrdata->vddpll);
+	}
+
+	pwrdata->vdda = devm_regulator_get(&pdev->dev, "vdda");
+	if (IS_ERR(pwrdata->vdda)) {
+		pr_err("%s: could not get vdda vreg, rc=%ld\n",
+			__func__, PTR_ERR(pwrdata->vdda));
+		return PTR_ERR(pwrdata->vdda);
 	}
 
 	ret = regulator_set_voltage(pwrdata->vdda, 1200000,
 		1200000);
 	if (ret) {
-	    PR_DISP_ERR("%s: set voltage failed on vdda vreg, rc=%d\n",
-	        __func__, ret);
-	    return ret;
+		pr_err("%s: set voltage failed on vdda vreg, rc=%d\n",
+			__func__, ret);
+		return ret;
 	}
 
-	pwrdata->vddpll = devm_regulator_get(&pdev->dev, "vddpll");
-	if (IS_ERR(pwrdata->vddpll)) {
-		PR_DISP_ERR("%s: could not get vddpll vreg, rc=%ld\n",
-			__func__, PTR_ERR(pwrdata->vddpll));
-		return PTR_ERR(pwrdata->vddpll);
-	}
-
-	ret = regulator_set_voltage(pwrdata->vddpll, 1800000,
-		1800000);
+	ret = regulator_set_voltage(pwrdata->vlcm3v, 3000000,
+			3000000);
 	if (ret) {
-	    PR_DISP_ERR("%s: set voltage failed on vddpll vreg, rc=%d\n",
-	        __func__, ret);
-	    return ret;
+		pr_err("%s: set voltage failed on vlcm3v vreg, rc=%d\n",
+			__func__, ret);
+		return ret;
 	}
 
-	pwrdata->lcmp5v = of_get_named_gpio(pdev->dev.of_node,
-						"htc,lcm_p5v-gpio", 0);
-	pwrdata->lcmn5v = of_get_named_gpio(pdev->dev.of_node,
-						"htc,lcm_n5v-gpio", 0);
+	pwrdata->lcm_bl_en = of_get_named_gpio(pdev->dev.of_node,
+						"htc,lcm_bl_en-gpio", 0);
 
 	ret = i2c_add_driver(&tps_65132_tx_i2c_driver);
 	if (ret < 0) {
-		PR_DISP_ERR("%s: FAILED to add i2c_add_driver ret=%x\n",
+		pr_err("[DISP] %s: FAILED to add i2c_add_driver ret=%x\n",
 			__func__, ret);
 	}
 	return 0;
 }
 
+
 static int htc_a11_regulator_deinit(struct platform_device *pdev)
 {
-	/* devm_regulator() will automatically free regulators
-	   while dev detach. */
-	/* nothing */
+	
 	return 0;
 }
 
@@ -268,55 +252,123 @@ int htc_a11_panel_reset(struct mdss_panel_data *pdata, int enable)
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
 
 	if (pdata == NULL) {
-		PR_DISP_ERR("%s: Invalid input data\n", __func__);
+		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
 
 	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
 
+	if (!gpio_is_valid(ctrl_pdata->disp_en_gpio)) {
+		pr_debug("%s:%d, reset line not configured\n",
+			   __func__, __LINE__);
+	}
 
 	if (!gpio_is_valid(ctrl_pdata->rst_gpio)) {
-		PR_DISP_DEBUG("%s:%d, reset line not configured\n",
+		pr_debug("%s:%d, reset line not configured\n",
 			   __func__, __LINE__);
 		return -EINVAL;
 	}
 
-	PR_DISP_DEBUG("%s: enable = %d\n", __func__, enable);
+	pr_debug("%s: enable = %d\n", __func__, enable);
 
 	if (enable) {
 		if (pdata->panel_info.first_power_on == 1) {
 			PR_DISP_INFO("reset already on in first time\n");
 			return 0;
 		}
-		if (pdata->panel_info.panel_id == PANEL_ID_KIWI_SHARP_HX) {
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			usleep_range(1000,1500);
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			usleep_range(1000,1500);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			msleep(25);
-		} else {
-			usleep_range(1000,1500);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			usleep_range(1000,2000);
-			gpio_set_value((ctrl_pdata->rst_gpio), 0);
-			usleep_range(15000,16000);
-			gpio_set_value((ctrl_pdata->rst_gpio), 1);
-			msleep(120);
-		}
-		if (ctrl_pdata->ctrl_state & CTRL_STATE_PANEL_INIT) {
-			PR_DISP_DEBUG("%s: Panel Not properly turned OFF\n",
-						__func__);
-			ctrl_pdata->ctrl_state &= ~CTRL_STATE_PANEL_INIT;
-			PR_DISP_DEBUG("%s: Reset panel done\n", __func__);
-		}
+
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		usleep_range(2000,2500);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+		usleep_range(100,150);
+		gpio_set_value((ctrl_pdata->rst_gpio), 1);
+		msleep(25);
 	} else {
-		usleep_range(1000,1500);
 		gpio_set_value((ctrl_pdata->rst_gpio), 0);
 	}
 
 	return 0;
+}
+
+static void htc_a11_bkl_en(struct mdss_panel_data *pdata, int enable)
+{
+	static int en = 0;
+	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
+	struct dsi_power_data *pwrdata = NULL;
+	u8 data = 0x00;
+	int ret =0;
+
+	if(en == enable)
+		return;
+	en = enable;
+
+	PR_DISP_INFO("%s:en=%d\n", __func__, enable);
+
+	if (pdata == NULL) {
+	        pr_err("%s: Invalid input data\n", __func__);
+	        return;
+	}
+
+	ctrl_pdata = container_of(pdata, struct mdss_dsi_ctrl_pdata,
+	                        panel_data);
+	pwrdata = ctrl_pdata->dsi_pwrctrl_data;
+
+	if(enable) {
+		gpio_set_value(pwrdata->lcm_bl_en, 1);
+		usleep_range(2000,2500);
+
+		
+		data = 0x02;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x50, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x50, 1, &data);
+
+		usleep_range(500,1000);
+
+		
+		data=0x09;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x01, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x01, 1, &data);
+
+		usleep_range(500,1000);
+
+		
+		data=0x70;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x02, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x02, 1, &data);
+
+		usleep_range(500,1000);
+
+		
+		data=0x14;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x05, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x05, 1, &data);
+
+		usleep_range(500,1000);
+
+		
+		data=0x04;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x00, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x00, 1, &data);
+
+		usleep_range(6000,8000);
+
+		
+		data=0xFF;
+		ret = platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x03, 1, &data);
+		if(ret)
+			platform_write_i2c_block(i2c_bus_adapter, 0x6C, 0x03, 1, &data);
+		usleep_range(500,1000);
+
+	} else {
+		gpio_set_value(pwrdata->lcm_bl_en, 0);
+	}
+
 }
 
 static int htc_a11_panel_power_on(struct mdss_panel_data *pdata, int enable)
@@ -325,11 +377,10 @@ static int htc_a11_panel_power_on(struct mdss_panel_data *pdata, int enable)
 
 	struct mdss_dsi_ctrl_pdata *ctrl_pdata = NULL;
 	struct dsi_power_data *pwrdata = NULL;
-	u8 avdd_level = 0x0F; //set to +-5.5V
 
 	PR_DISP_INFO("%s: en=%d\n", __func__, enable);
 	if (pdata == NULL) {
-		PR_DISP_ERR("%s: Invalid input data\n", __func__);
+		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
 	}
 
@@ -338,207 +389,151 @@ static int htc_a11_panel_power_on(struct mdss_panel_data *pdata, int enable)
 	pwrdata = ctrl_pdata->dsi_pwrctrl_data;
 
 	if (!pwrdata) {
-		PR_DISP_ERR("%s: pwrdata not initialized\n", __func__);
+		pr_err("%s: pwrdata not initialized\n", __func__);
 		return -EINVAL;
 	}
 
 	if (enable) {
-		if (pdata->panel_info.panel_id == PANEL_ID_KIWI_SHARP_HX){
-			PR_DISP_INFO("%s: sharp power on\n", __func__);
-			ret = regulator_set_optimum_mode(pwrdata->vdd, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vddio set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-			ret = regulator_set_optimum_mode(pwrdata->vddio, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vddio set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			ret = regulator_set_optimum_mode(pwrdata->vdda, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vdda set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			ret = regulator_set_optimum_mode(pwrdata->vddpll, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vddpll set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			/*enable 1v8*/
-			ret = regulator_enable(pwrdata->vddio);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-			usleep_range(1000,2000);
-			/*enable 3v*/
-			ret = regulator_enable(pwrdata->vdd);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-			usleep_range(12000,13000);
-
-			/*enable 1v8*/
-			ret = regulator_enable(pwrdata->vddpll);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-			/*ENABLE 1V2*/
-			ret = regulator_enable(pwrdata->vdda);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-		} else {
-			PR_DISP_INFO("%s: LG power on\n", __func__);
-			ret = regulator_set_optimum_mode(pwrdata->vddio, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vddio set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			ret = regulator_set_optimum_mode(pwrdata->vdda, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vdda set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			ret = regulator_set_optimum_mode(pwrdata->vddpll, 100000);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vddpll set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-
-			/*enable 1v8*/
-			ret = regulator_enable(pwrdata->vddio);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-			usleep_range(5000,5100);
-			gpio_set_value(pwrdata->lcmp5v, 1);
-			platform_write_i2c_block(i2c_bus_adapter,0x7C,0x00, 0x01, &avdd_level);
-			platform_write_i2c_block(i2c_bus_adapter,0x7C,0x01, 0x01, &avdd_level);
-			usleep_range(4000,4100);
-			gpio_set_value(pwrdata->lcmn5v, 1);
-
-
-			usleep_range(12000,13000);
-
-			/*enable 1v8*/
-			ret = regulator_enable(pwrdata->vddpll);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-
-			/*ENABLE 1V2*/
-			ret = regulator_enable(pwrdata->vdda);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to enable regulator.\n",
-					__func__);
-				return ret;
-			}
-			msleep(50);
+		ret = regulator_set_optimum_mode(pwrdata->vddio, 100000);
+		if (ret < 0) {
+			pr_err("%s: vddio set opt mode failed.\n",
+				__func__);
+			return ret;
 		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vddpll, 100000);
+		if (ret < 0) {
+			pr_err("%s: vddpll set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vdda, 100000);
+		if (ret < 0) {
+			pr_err("%s: vdda set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vlcm3v, 100000);
+		if (ret < 0) {
+			pr_err("%s: vlcm3v set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+		usleep_range(12000,12500);
+
+		
+		ret = regulator_enable(pwrdata->vddio);
+		if (ret) {
+			pr_err("%s: Failed to enable regulator.\n",
+				__func__);
+			return ret;
+		}
+		usleep_range(1000,1500);
+
+		ret = regulator_enable(pwrdata->vlcm3v);
+		if (ret) {
+			pr_err("%s: Failed to enable regulator : vlcm3v.\n",
+				__func__);
+			return ret;
+		}
+		usleep_range(1000,1500);
+
+		ret = regulator_enable(pwrdata->vddpll);
+		if (ret) {
+			pr_err("%s: Failed to enable regulator.\n",
+				__func__);
+			return ret;
+		}
+		usleep_range(1000,1500);
+
+		
+		ret = regulator_enable(pwrdata->vdda);
+		if (ret) {
+			pr_err("%s: Failed to enable regulator : vdda.\n",
+				__func__);
+			return ret;
+		}
+
+		usleep_range(15000,20000);
 	} else {
-		htc_a11_panel_reset(pdata, 0);
-		msleep(70);
+
+		usleep_range(2000,2500);
+		gpio_set_value((ctrl_pdata->rst_gpio), 0);
+
+		
 		ret = regulator_disable(pwrdata->vdda);
 		if (ret) {
-			PR_DISP_ERR("%s: Failed to disable regulator vdda.\n",
+			pr_err("%s: Failed to disable regulator.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_disable(pwrdata->vlcm3v);
+		if (ret) {
+			pr_err("%s: Failed to disable regulator : vlcm3v.\n",
 				__func__);
 			return ret;
 		}
 
 		ret = regulator_disable(pwrdata->vddpll);
 		if (ret) {
-			PR_DISP_ERR("%s: Failed to disable regulator vddpll.\n",
+			pr_err("%s: Failed to disable regulator.\n",
 				__func__);
 			return ret;
 		}
 
-		if (pdata->panel_info.panel_id == PANEL_ID_KIWI_SHARP_HX){
-			ret = regulator_disable(pwrdata->vdd);
-			if (ret) {
-				PR_DISP_ERR("%s: Failed to disable regulator vdd.\n",
-					__func__);
-				return ret;
-			}
-		}
-
-		gpio_set_value(pwrdata->lcmn5v, 0);
-		usleep_range(5000,5100);
-		gpio_set_value(pwrdata->lcmp5v, 0);
-		usleep_range(5000,5100);
+		
 		ret = regulator_disable(pwrdata->vddio);
 		if (ret) {
-			PR_DISP_ERR("%s: Failed to disable regulator vddio.\n",
-				__func__);
-			return ret;
-		}
-		if (pdata->panel_info.panel_id == PANEL_ID_KIWI_SHARP_HX){
-			ret = regulator_set_optimum_mode(pwrdata->vdd, 100);
-			if (ret < 0) {
-				PR_DISP_ERR("%s: vdd_vreg set opt mode failed.\n",
-					__func__);
-				return ret;
-			}
-		}
-		ret = regulator_set_optimum_mode(pwrdata->vddio, 100);
-		if (ret < 0) {
-			PR_DISP_ERR("%s: vdd_io_vreg set opt mode failed.\n",
-				__func__);
-			return ret;
-		}
-		ret = regulator_set_optimum_mode(pwrdata->vdda, 100);
-		if (ret < 0) {
-			PR_DISP_ERR("%s: vdda_vreg set opt mode failed.\n",
-				__func__);
-			return ret;
-		}
-		ret = regulator_set_optimum_mode(pwrdata->vddpll, 100);
-		if (ret < 0) {
-			PR_DISP_ERR("%s: vddpll_vreg set opt mode failed.\n",
+			pr_err("%s: Failed to disable regulator : vddio.\n",
 				__func__);
 			return ret;
 		}
 
+		usleep_range(2000,2500);
+
+		ret = regulator_set_optimum_mode(pwrdata->vddio, 100);
+		if (ret < 0) {
+			pr_err("%s: vdd_io_vreg set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vlcm3v, 100);
+		if (ret < 0) {
+			pr_err("%s: vlcm3v_vreg set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vddpll, 100);
+		if (ret < 0) {
+			pr_err("%s: vddpll_vreg set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
+
+		ret = regulator_set_optimum_mode(pwrdata->vdda, 100);
+		if (ret < 0) {
+			pr_err("%s: vdda_vreg set opt mode failed.\n",
+				__func__);
+			return ret;
+		}
 	}
 	PR_DISP_INFO("%s: en=%d done\n", __func__, enable);
 
 	return 0;
 }
 
+
 static struct mdss_dsi_pwrctrl dsi_pwrctrl = {
 	.dsi_regulator_init = htc_a11_regulator_init,
 	.dsi_regulator_deinit = htc_a11_regulator_deinit,
 	.dsi_power_on = htc_a11_panel_power_on,
 	.dsi_panel_reset = htc_a11_panel_reset,
+	.bkl_config = htc_a11_bkl_en,
 };
 
 static struct platform_device dsi_pwrctrl_device = {
